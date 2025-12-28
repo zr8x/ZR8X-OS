e72367f87e58ead48ca5734fefb08e09cd6bef5a
zr8x <realzr8x@gmail.com>
2025-12-28 15:26:01 -0500

    Troubleshooting on windows

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,46 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0E, 0x0A
+
+puts:
+	push si
+	push ax
+
+
+.loop:
+	lodsb
+	or al, al
+	jz .done
+
+	mov ah, 0x0e
+	int 0x10
+
+	jmp .loop
+
+.done:
+	pop ax
+	pop si
+	ret
+
 main:
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+
+	mov ss, ax
+	mov sp, 0x7C00
+
+
+	mov si, msg_hello
+	call puts
+
 	hlt
 
 .halt:
 	jmp .halt
+
+msg_hello: db "Hello world!", ENDL, 0
 
 
 times 510-($-$$) db 0
